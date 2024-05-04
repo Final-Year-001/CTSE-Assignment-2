5a0e5f3948ea040636f2ca18480090f62ea4beab
nisal <it20081034@my.sliit.lk>
2024-05-04 14:54:51 +0530

    nisal check 2

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 router.use("/api/v1/songs", songRouter);
 router.use("/api/v1/playlists", playlistRouter);
 router.get("/health", (req, res) => {
-    res.json({ "message" : "health is grate. hi."})
-})
+  res.json({ message: "health is grate. hi hi updated." });
+});
 
 export default router;
