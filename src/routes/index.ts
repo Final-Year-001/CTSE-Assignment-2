f6ce774a496a88f13e6e587ea3c781c9d9668093
Dehemi Weerakkody <dehemiweerakkodi2@gmail.com>
2024-05-04 13:30:28 +0530

    Add health endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,5 +6,8 @@ const router = express.Router();
 
 router.use("/api/v1/songs", songRouter);
 router.use("/api/v1/playlists", playlistRouter);
+router.get("/health", (req, res) => {
+    res.json({ "message" : "health is grate."})
+})
 
 export default router;
